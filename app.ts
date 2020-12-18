45f19c13d5fe138dd72ea0d6a86e90f4cefb5ac5
Diego Pagini <diegopagini777@gmail.com>
2020-12-18 06:38:07 -0300

    terminando

--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
-(() => {})();
+(() => {
+	const sumar = (a: number, b: number): number => {
+		return a + b;
+	};
+
+	const nombre = (): string => {
+		return 'Hola Diego';
+	};
+
+	const obtenerSalario = (): Promise<string> => {
+		return new Promise((resolve, reject) => {
+			resolve('Diego');
+			reject('Nooo');
+		});
+	};
+
+	obtenerSalario()
+		.then((a) => console.log(a))
+		.catch((a) => {
+			console.log(a);
+		});
+})();
